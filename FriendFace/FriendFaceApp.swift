2fe75a9fc1119a3c5810b0fadbc858e0aefa9f9e
fischej <61101706+fischej@users.noreply.github.com>
2022-07-29 13:11:20 -0700

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Jeff Fischer on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
